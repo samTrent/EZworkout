308761811670e15e0a658757d4467f4517d1150b
Austin Kunz <kun13004@byui.edu>
2016-10-12 15:30:27 -0600

    i'm the best

--- Test.java
+++ Test.java
@@ -2,8 +2,6 @@ class Test {
 	public static void main(String[] args) {
 		System.out.println("Hi Guys");
 		
-		System.out.println("I want it to say this");
-
-		System.out.println("I made a change");
+		System.out.println("I'm gonna rest cuz I'm the best");
 	}
 }
